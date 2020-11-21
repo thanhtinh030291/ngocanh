8f62f5fff703c547215746f458f358e16d9808d0
thanhtinh030291 <43638255+thanhtinh030291@users.noreply.github.com>
2020-11-21 17:13:45 +0700

    update

--- resources/views/computer/home/master.blade.php
+++ resources/views/computer/home/master.blade.php
@@ -590,19 +590,8 @@ function topFunction() {
 </script>
  <button onclick="topFunction()" id="myBtn" title="Về đầu trang"><b class="fa fa-arrow-up"></b></button>
 <div class="hidden-xs">
-    <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/59883393dbb01a218b4db159/default';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })
-();
-</script>
+<!--Start of Tawk.to Script-->
+
 <!--End of Tawk.to Script-->
 </div>
 </body>
