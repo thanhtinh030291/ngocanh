87effba25bb43305e16653e2bdbcdfe691093fd8
thanhtinh030291 <43638255+thanhtinh030291@users.noreply.github.com>
2020-11-22 09:58:49 +0700

    update

--- resources/views/computer/home/master.blade.php
+++ resources/views/computer/home/master.blade.php
@@ -98,11 +98,35 @@
 <b></b>
 <body id="page-top">
  
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v9.0'
+        });
+      };
 
+      (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    <!-- Your Chat Plugin code -->
+    <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="787066881651278"
+        theme_color="#67b868"
+        logged_in_greeting="Cảm ơn bạn đã nhắn tin cho chúng tôi"
+        logged_out_greeting="Cảm ơn bạn đã nhắn tin cho chúng tôi">
+    </div>
     {!! (!empty($contact)?$contact->google_analyst:"") !!}
 
 
-    <div id="fb-root"></div>
     <script>(function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
@@ -590,34 +614,7 @@ function topFunction() {
 </script>
  <button onclick="topFunction()" id="myBtn" title="Về đầu trang"><b class="fa fa-arrow-up"></b></button>
 
-<!--Start of Tawk.to Script-->
-<!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      xfbml            : true,
-      version          : 'v9.0'
-    });
-  };
 
-  (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<!-- Your Chat Plugin code -->
-<div class="fb-customerchat"
-  attribution=setup_tool
-  page_id="787066881651278"
-theme_color="#67b868"
-logged_in_greeting="Cảm ơn bạn đã nhắn tin cho chúng tôi"
-logged_out_greeting="Cảm ơn bạn đã nhắn tin cho chúng tôi">
-</div>
-<!--End of Tawk.to Script-->
 
 </body>
 
